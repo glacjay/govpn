60da4763fc608a6aaee19183080a61948d64e88f
Jay True <glacjay@gmail.com>
2011-07-05 19:27:16 +0800

    basic structure

---
+++ Makefile
@@ -0,0 +1,8 @@
+include $(GOROOT)/src/Make.inc
+
+TARG = govpn
+
+GOFILES = \
+	main.go \
+
+include $(GOROOT)/src/Make.cmd
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+type context struct {
+	firstTime bool
+}
+
+func main() {
+	var c context
+	c.firstTime = true
+}
