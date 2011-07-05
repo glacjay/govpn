610b1992250df4f95ab669c9ad87f1c08f89fe1c
Jay True <glacjay@gmail.com>
2011-07-05 19:31:45 +0800

    more basic structure

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
+import (
+	"os"
+)
+
 type context struct {
 	firstTime bool
+}
+
+func initStatic() os.Error {
+	return nil
+}
+
+func exitStatic() {
 }
 
 func main() {
 	var c context
 	c.firstTime = true
+	if err := initStatic(); err == nil {
+	}
+	exitStatic()
 }
