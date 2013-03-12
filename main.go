cb12116e12a4500540b6965bcba2d17f3a4b0815
Jay True <glacjay@gmail.com>
2013-03-12 18:49:46 +0800

    reading static secret file

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"flag"
+	"fmt"
 	"github.com/glacjay/govpn/tun"
 	"log"
 	"net"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -12,10 +16,66 @@ import (
 var (
 	flagVirtAddr   = flag.String("virt-addr", "10.8.0.1/24", "virtual IP address of my TUN/TAP device")
 	flagRemoteHost = flag.String("remote-host", "localhost", "remote peer's hostname or IP address")
+	flagSecretFile = flag.String("secret-file", "", "name of file which contains static secret keys")
+	flagSecretDir  = flag.Int("secret-direction", -1, "direction of static secret keys: 0 for normal, 1 for inverse, and default (unset) for bidirectional")
 )
+
+const (
+	maxCipherKeyLen = 64
+	maxHMacKeyLen   = 64
+
+	staticKeyHead = "-----BEGIN OpenVPN Static key V1-----"
+	staticKeyFoot = "-----END OpenVPN Static key V1-----"
+)
+
+type key2 struct {
+	count int
+	keys  [2]key
+}
+
+type key struct {
+	cipher [maxCipherKeyLen]byte
+	hmac   [maxHMacKeyLen]byte
+}
 
 func main() {
 	flag.Parse()
+
+	var keys *key2
+	var encryptIndex, decryptIndex int
+	if *flagSecretFile != "" {
+		content := readSecretFile(*flagSecretFile)
+		keys = &key2{count: len(content) / (maxCipherKeyLen + maxHMacKeyLen)}
+		index := 0
+		for i := 0; i < keys.count; i++ {
+			copy(keys.keys[i].cipher[:], content[index:index+maxCipherKeyLen])
+			index += maxCipherKeyLen
+			copy(keys.keys[i].hmac[:], content[index:index+maxHMacKeyLen])
+			index += maxHMacKeyLen
+		}
+		switch *flagSecretDir {
+		case -1:
+			if keys.count < 1 {
+				log.Fatal("Malformed secret file: contains less than 1 complete key for bidirection")
+			}
+			encryptIndex = 0
+			decryptIndex = 0
+		case 0:
+			if keys.count < 2 {
+				log.Fatal("Malformed secret file: contains less than 2 complete key for normal direction")
+			}
+			encryptIndex = 0
+			decryptIndex = 1
+		case 1:
+			if keys.count < 2 {
+				log.Fatal("Malformed secret file: contains less than 2 complete key for inverse direction")
+			}
+			encryptIndex = 1
+			decryptIndex = 0
+		default:
+			log.Fatal("Invalid direction setup: %v", *flagSecretDir)
+		}
+	}
 
 	remoteAddrs, err := net.LookupIP(*flagRemoteHost)
 	if err != nil || len(remoteAddrs) == 0 {
@@ -74,4 +134,39 @@ func linkToTunLoop(conn *net.UDPConn, tunDevice *tun.Tun) {
 			return
 		}
 	}
+}
+
+func readSecretFile(filename string) []byte {
+	file, err := os.Open(*flagSecretFile)
+	if err != nil {
+		log.Fatalf("Failed to open secret file '%s': %v", *flagSecretFile, err)
+	}
+	scanner := bufio.NewScanner(file)
+	inData := false
+	content := &bytes.Buffer{}
+	lineno := 0
+	for scanner.Scan() {
+		lineno++
+		if scanner.Text() == staticKeyHead {
+			inData = true
+			continue
+		}
+		if scanner.Text() == staticKeyFoot {
+			break
+		}
+		if inData {
+			if len(scanner.Text())%2 == 1 {
+				log.Fatalf("Malformed secret file: line %d has odd characters", lineno)
+			}
+			for i := 0; i < len(scanner.Text())/2; i++ {
+				var c byte
+				_, err := fmt.Sscanf(scanner.Text()[i:i+2], "%02x", &c)
+				if err != nil {
+					log.Fatalf("Malformed secret file: line %d has invalid hex numbers", lineno)
+				}
+				content.WriteByte(c)
+			}
+		}
+	}
+	return content.Bytes()
 }
